281107e45436b23450063f7a8730dac39a4a58c8
aksoftware98 <aksoftware98@hotmail.com>
2020-03-12 01:52:10 +0200

    Add Real-Time to Blazor Webassembly with SignalR

---
+++ src/BlazorRealTime.Server/Controllers/NotificationsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorRealTime.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationsController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromQuery]string title)
+        {
+            await _hubContext.Clients.All.SendAsync("notification", $"{DateTime.Now}: {title}");
+            return Ok("Notification has been sent successfully!"); 
+        }
+
+    }
+}
---
+++ src/BlazorRealTime.Server/NotificationHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRealTime.Server
+{
+    public class NotificationHub : Hub
+    {
+
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+    }
+}
